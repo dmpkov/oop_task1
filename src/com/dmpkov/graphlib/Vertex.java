fc1e5baaa5af5f0c03c18a9ea71dcd27062d1b39
dmpkov <dpervakov10@gmail.com>
2025-10-15 23:21:01 +0300

    Make basic Vertex class

--- src/com/dmpkov/graphlib/Vertex.java
+++ src/com/dmpkov/graphlib/Vertex.java
@@ -1,4 +1,15 @@
 package com.dmpkov.graphlib;
 
-public class Vertex {
+public class Vertex<T> {
+    private final T value;
+
+    public Vertex(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+
 }
